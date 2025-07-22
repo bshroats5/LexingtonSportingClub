8bd26d19296bc0e15e1e795e9500fad8a8d6fa63
Bret Shroats <bshroats5@gmail.com>
2025-07-21 23:47:24 -0400

    Refactor DataFrame to flatten multi-index columns
    Updated the DataFrame to flatten multi-index columns by joining column names and resetting the index. This simplifies further data manipulation and ensures consistent column headers.

--- LSC.py
+++ LSC.py
@@ -5,3 +5,10 @@ import pandas as pd
 url_df = 'https://fbref.com/en/squads/7622315f/Lexington-SC-Stats'
 df = pd.read_html(url_df)
 print(df)
+
+# creating a data with the same headers but without multi indexing
+df.columns = [' '.join(col).strip() for col in df.columns]
+
+df = df.reset_index(drop=True)
+df.head()
+print(df)
